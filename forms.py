467dd7eea8621d07373b865d64804bf6712b04dd
StevenYang <52616166+StevenYangForGit@users.noreply.github.com>
2022-09-04 20:42:29 +0800

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,50 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from models import User
+
+class RegisterForm(FlaskForm):
+    username = StringField('Usernamae', validators=[DataRequired(), Length(min=6, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
+    confirm = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('Username already token, please choose another one.')
+        
+    def validate_email(self, email):
+        email = User.query.filter_by(email = email.data).first()
+        if email:
+            raise ValidationError('Email already token, please choose another one.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Usernamae', validators=[DataRequired(), Length(min=6, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
+    remember = BooleanField('Remember')
+    submit = SubmitField('Sign In')
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Send')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email = email.data).first()
+        if not email:
+            raise ValidationError('Email not exists.')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=20)])
+    confirm = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+class PostTweetForm(FlaskForm):
+    text = TextAreaField('Say Something ...', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Post Text')
+
+class UploadPhotoForm(FlaskForm):
+    photo = FileField(validators=[DataRequired()])
+    submit = SubmitField('Upload')
---
+++ models.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from email.policy import default
+from flask import current_app
+from flask_login import UserMixin
+import jwt
+from app import app, db, login
+
+@login.user_loader
+def user_loader(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    avatar_img = db.Column(db.String(120), default='/static/asset/default-avatar.jpg', nullable=False)
+    posts = db.relationship('Post', backref=db.backref('author',lazy=True))
+    followed = db.relationship(
+        'User', 
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id==id),
+        secondaryjoin=(followers.c.followed_id==id),
+        backref=db.backref('followers',lazy=True), lazy=True
+        )
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+    def generate_reset_password_token(self):
+        return jwt.encode({'id': self.id}, current_app.config['SECRET_KEY'], algorithm='HS256')
+    
+    @staticmethod
+    def check_reset_password_token(token):
+        try:
+            data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms='HS256')
+            return User.query.filter_by(id=data['id']).first()
+        except:
+            return
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    
+    def unfollow(self,user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.count(user) > 0
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
